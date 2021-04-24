b5b66bfb5260b7cb5fec9212369236cf86dc8ec1
Hajar HKR <elhakourhajar@gmail.com>
2021-04-24 22:35:03 +0000

    still have a problem of asynchronous loading + added onScrolling model change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { Suspense, useRef} from 'react'
+import React, { Suspense, useRef, useEffect} from 'react'
 import './App.scss';
 import Cubes from './Cubes'
 import Header from './components/Header'
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Html } from '@react-three/drei'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Html, useGLTFLoader } from '@react-three/drei'
 import { Section } from './section';
 import { useGLTF } from '@react-three/drei';
-import { Light } from 'three';
 
 
-const Model = () => {
-  const gltf = useGLTF('/scene.gltf', true)
+//This is page state
+import state from './state'
+
+
+const Model = ({ modelPath }) => {
+  const gltf = useGLTF(modelPath)
   return <primitive object={gltf.scene} dispose={null} />
 
 
 }
+
 
 const Lights = () => {
   return (
@@ -27,29 +32,26 @@ const Lights = () => {
   )
 };
 
-const HTMLcontent = () => {
-   //ref to target the mesh
-   const ref = useRef();
+const HTMLcontent = ({domContent, children, modelPath, positionY }) => {
+  // //ref to target the mesh
+  // const ref = useRef();
 
-   //useFrame allows us to re-render/update rotation on each frame
-   useFrame(() => (ref.current.rotation.y += 0.01));
- 
+  // //useFrame allows us to re-render/update rotation on each frame
+  // useFrame(() => (ref.current.rotation.y += 0.01));
+
 
 
   return (
     <Section
       factor={1.5}
       offset={1}>
-      <group position={[0, 200, 0]}>
+      <group position={[0, positionY, 0]}>
         <mesh
           position={[0, 35, 0]}>
-          <Model />
+          <Model modelPath={modelPath} />
+
         </mesh>
-        <Html fullscreen>
-          <div className='container'>
-            <h1 className='title'>Hello</h1>
-          </div>
-        </Html>
+        <Html fullscreen>{children}</Html>
       </group>
     </Section>
 
@@ -62,7 +64,10 @@ const HTMLcontent = () => {
 
 function App() {
 
-
+  const domContent = useRef();
+  const scrollArea = useRef();
+  const onScroll = (e) => (state.top.current =e.target.scrollTop)
+  useEffect(() => void onScroll({ target :scrollArea.current}), [])
   return (
     <>
       <Header />
@@ -72,11 +77,34 @@ function App() {
 
         <Lights />
         <Suspense fallback={null}>
+          <HTMLcontent
+            domContent={domContent}
+            modelPath="/scene1.gltf"
+            positionY={200}>
 
-          <HTMLcontent />
+            <div className='container'>
+              <h1 className='title'>Hello</h1>
+            </div>
+
+          </HTMLcontent>
+          <HTMLcontent
+            domContent={domContent}
+            modelPath="/scene2.gltf"
+            positionY={0}>
+
+            <div className='container'>
+              <h1 className='title'>Bye</h1>
+            </div>
+
+          </HTMLcontent>
+
         </Suspense>
 
       </Canvas>
+      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        <div style={{ position: 'sticky', top: 0 }} ref={domContent}></div>
+        <div style={{ height: `${state.sections * 100}vh` }}></div>
+      </div>
       {/* <Cubes/> */}
     </>
   );
--- src/Cubes.js
+++ src/Cubes.js
@@ -50,6 +50,7 @@ export default class Cubes extends React.Component {
           colorManagement
           shadowMap
           camera={{ position: [-5, 2, 10], fov: 60 }}>
+
           {/* This light makes things look pretty */}
           <ambientLight intensity={0.3} />
           {/* Our main source of light, also casting our shadow */}
@@ -77,12 +78,14 @@ export default class Cubes extends React.Component {
               <planeBufferGeometry attach='geometry' args={[100, 100]} />
               <shadowMaterial attach='material' opacity={0.3} />
             </mesh>
+
             <SpinningMesh
               position={[0, 1, 0]}
               color='lightblue'
               args={[3, 2, 1]}
               speed={2}
             />
+            
             <SpinningMesh position={[-2, 1, -5]} color='pink' speed={6} />
             <SpinningMesh position={[5, 1, -2]} color='pink' speed={6} />
           </group>
